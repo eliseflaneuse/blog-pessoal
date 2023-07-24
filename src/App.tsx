effb270852d36ec34c4f829ef33b456b25b5321d
eliseflaneuse <eliseflaneuse@gmail.com>
2023-07-24 13:58:32 -0300

    Changes in the App and Home components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
-import Home from './components/Home/Home';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
+import Card from "./components/Card/Card";
+import Contador from "./components/Contador/Contador";
+import Home from "./components/Home/Home";
+import Task from "./components/Task/Task";
 
 function App() {
   return (
-   <Home />
+    <>
+      <Home />
+    </>
   );
 }
 
 export default App;
+
+/*Props
+   <>
+    <Card   title="Component Card"
+            description="Este é um componente Card que recebe props."/>
+  </>
+  */
+
+/*
+  useState and useEffect
+  <>
+  <Contador />
+  <Task />
+  </>
+  */
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import './Home.css';
+import { useState } from 'react';
 
-function Home(){
-    return (
-        <>
-            <h1 >Home</h1>
-           
-        </>
-    );
+function Home() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  return (
+    <div>
+      {loggedIn ? (
+        <h1>Bem-vindo de volta!</h1>
+      ) : (
+        <button onClick={() => setLoggedIn(true)}>Entrar</button>
+      )}
+    </div>
+  );
 }
-
 export default Home;
